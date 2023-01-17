50310c70daaffdee5189e17c7509e2178eedd95e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-17 22:15:43 +0700

    update-Components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,50 @@ import React from "react";
 import styles from "./styles/Global";
 
 import assets from './assets';
+import { Download, Features, SectionWrapper } from './components';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <SectionWrapper 
+        title="You own 
+        store of Nifty NFTs. Start Selling & Growing"
+        description="Buy, store, 
+        collect NFTS, exchange & earn crypto. 
+        Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper 
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper 
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper 
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
+        <span className="bold">
+          BaoNguyenDeV
+        </span>
+        </p>
+      </div>
+    </>
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import styles from '../styles/Global';
 
-const Button = () => {
+const Button = ({ assetUrl, link }) => {
   return (
-    <div>Button</div>
+    <>
+      <div 
+      className={styles.btnBlack}
+      onClick={() => window.open(link, "_blank")}
+    >
+      <img src={assetUrl} alt="expo_icon" className={styles.btnIcon} />
+      <div className="flex flex-col justify-start ml-4">
+        <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
+        <p className={`${styles.btnText} font-bold text-sm`}>
+          Expo Store
+        </p>
+      </div>
+    </div>
+    </>
   )
 }
 
